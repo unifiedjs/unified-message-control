d85462e601878358bc4ee246a7e1ae8424c042d1
Christian Murphy <Christian.Murphy.42@gmail.com>
2020-01-24 11:25:29 -0700

    typings: refactor to support unified
    also:
    * removes nullish
    * adds comments
    * adds more type tests
    * clarifies which props can be used with reset

--- index.d.ts
+++ index.d.ts
@@ -1,46 +1,70 @@
-// TypeScript Version: 3.7
+// TypeScript Version: 3.3
 
+import {Node} from 'unist'
+import {VFile} from 'vfile'
+import {Transformer} from 'unified'
 import {Test} from 'unist-util-is'
-import * as unist from 'unist'
-
-type Nullish = undefined | null
 
 declare namespace messageControl {
-  /** A comment marker. */
-  interface Marker<N extends unist.Node> {
-    /** name of marker */
+  /**
+   * A comment marker.
+   */
+  interface Marker<N extends Node> {
+    /**
+     * name of marker
+     */
     name: string
-    /** value after name */
+
+    /**
+     * value after name
+     */
     attributes: string
-    /** parsed attributes */
+
+    /**
+     * parsed attributes
+     */
     parameters: Record<string, unknown>
-    /** reference to given node */
+
+    /**
+     * reference to given node
+     */
     node: N
   }
 
-  /** parse a possible comment marker node to a Marker */
-  interface MarkerFn<N extends unist.Node> {
-    (node: N): Marker<N> | Nullish
-  }
+  /**
+   * parse a possible comment marker node to a Marker
+   */
+  type MarkerParser<N extends Node> = <N extends Node>(
+    node: N
+  ) => Marker<N> | null
 
-  interface MessageControlOptionsWithReset<T extends unist.Node>
+  interface MessageControlOptionsWithReset<T extends Node>
     extends BaseMessageControlOptions<T> {
-    // must be explicitly set to `true` because reset only defaults
-    // to `false`.
+    /**
+     * whether to treat all messages as turned off initially
+     */
     reset: true
-    disable?: string[]
-    enable?: unknown
+
+    /**
+     * list of `ruleId`s to initially turn on.
+     */
+    enable?: string[]
   }
 
-  interface MessageControlOptionsWithoutReset<T extends unist.Node>
+  interface MessageControlOptionsWithoutReset<T extends Node>
     extends BaseMessageControlOptions<T> {
-    reset?: false | Nullish
+    /**
+     * whether to treat all messages as turned off initially
+     */
+    reset?: false
 
-    enable?: string[] | Nullish
-    disable?: unknown
+    /**
+     * list of `ruleId`s to turn off
+     */
+    disable?: string[]
   }
 
-  interface BaseMessageControlOptions<T extends unist.Node> {
+  interface BaseMessageControlOptions<T extends Node> {
     /**
      * Name of markers that can control the message sources.
      *
@@ -49,13 +73,18 @@ declare namespace messageControl {
      * `<!--alpha ignore-->`
      */
     name: string
-    /** test for possible markers */
-    test: Test<T>
+
     /**
-     * parse a possible marker to a comment marker obejct (Marker)
+     * test for possible markers
+     */
+    test: Test<T>
+
+    /**
+     * parse a possible marker to a comment marker object (Marker)
      * if possible the marker isn't a marker, should return `null`.
      */
-    marker: MarkerFn<T>
+    marker: MarkerParser<T>
+
     /**
      * list of allowed `ruleId`s. When given a warning is shown
      * when someone tries to control an unknown rule.
@@ -65,32 +94,26 @@ declare namespace messageControl {
      *
      * `<!--alpha ignore charlie-->
      */
-    known?: string[] | Nullish
-    /**
-     * whether to treat all messages as turned off initially
-     * defeaults to false.
-     */
-    reset?: boolean | Nullish
-    /**
-     * list of `ruleId`s to initially turn on if `reset: true`.
-     */
-    enable?: string[] | Nullish | unknown
-    /** list of `ruleId`s to turn off if `reset: false` */
-    disable?: string[] | Nullish | unknown
+    known?: string[]
+
     /**
      * Sources that can be controlled with `name` markers.
-     * defaults to MessageControlOptions.name
+     *
+     * @defaultValue `MessageControlOptions.name`
      */
     sources?: string | string[]
   }
 
-  type MessageControlOptions<T extends unist.Node> =
+  type MessageControlOptions<T extends Node> =
     | MessageControlOptionsWithoutReset<T>
     | MessageControlOptionsWithReset<T>
 }
 
-declare function messageControl<T extends unist.Node>(
-  opts: messageControl.MessageControlOptions<T>
-): void
+/**
+ * Enable, disable, and ignore messages with unified.
+ */
+declare function messageControl(
+  options?: messageControl.MessageControlOptions<Node>
+): Transformer
 
 export = messageControl
--- type-test.ts
+++ type-test.ts
@@ -3,12 +3,133 @@
  */
 
 import control from 'unified-message-control'
+import unified, {Attacher} from 'unified'
 import {Node} from 'unist'
 
-const opts: control.MessageControlOptions<Node> = {
+// $ExpectError
+control({})
+
+control({
   name: 'foo',
   marker: (n: Node) => null,
   test: 'html'
-}
+})
 
-control(opts)
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  known: ['rule-1', 'rule-2']
+})
+
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  sources: 'example'
+})
+
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  sources: ['one', 'two']
+})
+
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: false,
+  disable: ['rule-id']
+})
+
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: false,
+  // $ExpectError
+  enable: ['rule-id']
+})
+
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: true,
+  enable: ['rule-id']
+})
+
+control({
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: true,
+  // $ExpectError
+  disable: ['rule-id']
+})
+
+// $ExpectError
+unified().use(control, {})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html'
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  known: ['rule-1', 'rule-2']
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  sources: 'example'
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  sources: ['one', 'two']
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: false,
+  disable: ['rule-id']
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: false,
+  // $ExpectError
+  enable: ['rule-id']
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: true,
+  enable: ['rule-id']
+})
+
+unified().use(control, {
+  name: 'foo',
+  marker: (n: Node) => null,
+  test: 'html',
+  reset: true,
+  // $ExpectError
+  disable: ['rule-id']
+})
